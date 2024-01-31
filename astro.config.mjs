485a52bf707604f024d9657aa8f5d7d2937e388e
Ales Zima <ales@cpex.cz>
2024-01-31 09:13:04 +0100

    Turning off SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   site: 'https://esosek.github.io',
   base: '/zabava-se-sarkou',
 });
