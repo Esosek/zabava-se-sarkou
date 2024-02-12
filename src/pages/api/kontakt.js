beff8c80528f7e16527ae33272bddd8bc8bd95d8
Ales Zima <ales@cpex.cz>
2024-02-12 17:34:15 +0100

    Improved email content

--- src/pages/api/kontakt.js
+++ src/pages/api/kontakt.js
@@ -7,6 +7,7 @@ export async function POST({ request }) {
   try {
     const requestData = await request.formData();
     const formData = getFormData(requestData);
+    console.log(formData);
 
     validateFormData(formData);
 
@@ -28,12 +29,12 @@ export async function POST({ request }) {
 
 function getFormData(data) {
   const result = {};
-  result.userName = escapeInput(data.get('name'));
-  result.userEmail = escapeInput(data.get('email'));
-  result.selectedService = escapeInput(data.get('service-select'));
-  result.selectedDate = escapeInput(data.get('date'));
-  result.location = escapeInput(data.get('location'));
-  result.userMessage = escapeInput(data.get('message'));
+  result.userName = escapeInput(data.get('name')).trim();
+  result.userEmail = escapeInput(data.get('email')).trim();
+  result.selectedService = escapeInput(data.get('service-select').trim());
+  result.selectedDate = escapeInput(data.get('date')).trim();
+  result.location = escapeInput(data.get('location')).trim();
+  result.userMessage = escapeInput(data.get('message')).trim();
   result.userSubscribe = data.get('subscribe');
   result.honeypot = data.get('user-number');
   return result;
@@ -60,11 +61,20 @@ function validateFormData({
 }) {
   const errors = [];
 
-  if (selectedService != null && selectedDate == null && location == null) {
-    errors.push('selected service is missing date and location');
+  if (selectedService !== 'zadny') {
+    if (selectedDate === '') {
+      errors.push('selected service is missing date');
+    }
+    if (location === '') {
+      errors.push('selected service is missing location');
+    }
   }
 
-  if (userName.trim().length < 3 || userName.trim().length > 50) {
+  if (selectedService === 'zadny' && userMessage.trim() === '') {
+    errors.push('message is empty');
+  }
+
+  if (userName.length < 3 || userName.length > 50) {
     errors.push('user name is too short or too long');
   }
 
@@ -76,18 +86,15 @@ function validateFormData({
     errors.push('selected service is not valid');
   }
 
-  if (selectedDate != null && !isDateValid(selectedDate)) {
+  if (selectedDate != '' && !isDateValid(selectedDate)) {
     errors.push('selected date is not valid');
   }
 
-  if (
-    (location != null && location.trim().length < 5) ||
-    location.trim().length > 200
-  ) {
+  if ((location != '' && location.length < 5) || location.length > 200) {
     errors.push('location is too short or long');
   }
 
-  if (userMessage.trim().length > 1500) {
+  if (userMessage.length > 1500) {
     errors.push('user message is too long');
   }
 
@@ -101,12 +108,12 @@ function validateFormData({
 
   function isEmailValid(email) {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+$/;
-    return regex.test(email.trim());
+    return regex.test(email);
   }
 
   function isServiceValid(service) {
     const regex = /[\p{L}-]/gu;
-    return regex.test(service.trim());
+    return regex.test(service);
   }
 
   function isDateValid(date) {
@@ -127,6 +134,10 @@ async function createEvent({
   selectedService,
   location,
 }) {
+  if (selectedService === 'zadny') {
+    return false;
+  }
+
   const dateStart = new Date(selectedDate);
   const dateEnd = new Date(dateStart);
   dateEnd.setDate(dateStart.getDate() + 1);
@@ -179,8 +190,8 @@ function setupMailTransporter() {
   return mailTransporter;
 }
 
-function prepareMailContent(
-  {
+function prepareMailContent(userData, isEventCreated) {
+  const {
     userName,
     userEmail,
     selectedService,
@@ -188,28 +199,46 @@ function prepareMailContent(
     location,
     userMessage,
     userSubscribe,
-  },
-  isEventCreated
-) {
+  } = userData;
+
+  const isOrder = selectedService !== 'zadny';
   const emailTo = import.meta.env.EMAIL_TO_ADDRESS;
-  const content = `<div>
-  <p><strong>Email:</strong> ${userEmail}</p>
-  <p><strong>Jméno:</strong> ${userName}</p>
-  <p><strong>Vybraný program:</strong> ${selectedService}</p>
-  <p><strong>Datum:</strong> ${selectedDate}${
+
+  const orderContent = `
+    <div>
+      <p><strong>Email:</strong> ${userEmail}</p>
+      <p><strong>Jméno:</strong> ${userName}</p>
+      <p><strong>Vybraný program:</strong> ${selectedService}</p>
+      <p><strong>Datum:</strong> ${selectedDate} ${
     !isEventCreated ? '- Událost nebyla v kalendáři vytvořena!' : ''
   }</p>
-  <p><strong>Adresa:</strong> ${location}</p>
-  <p><strong>Zpráva:</strong> ${userMessage}</p>
-  <p><strong>Přihlášení k odběru:</strong> ${
-    userSubscribe === 'on' ? 'ano' : 'ne'
-  }</p>
-  </div>`;
+      <p><strong>Adresa:</strong> ${location}</p>
+      <p><strong>Zpráva:</strong> ${userMessage}</p>
+      <p><strong>Přihlášení k odběru:</strong> ${
+        userSubscribe === 'on' ? 'ano' : 'ne'
+      }</p>
+    </div>
+  `;
+
+  const messageContent = `
+    <div>
+      <p><strong>Email:</strong> ${userEmail}</p>
+      <p><strong>Jméno:</strong> ${userName}</p>
+      <p><strong>Zpráva:</strong> ${userMessage}</p>
+      <p><strong>Přihlášení k odběru:</strong> ${
+        userSubscribe === 'on' ? 'ano' : 'ne'
+      }</p>
+    </div>
+  `;
+
+  const htmlContent = isOrder ? orderContent : messageContent;
 
   return {
     from: userEmail,
     to: emailTo,
-    subject: `Zpráva z webu od ${userName}`,
-    html: content,
+    subject: isOrder
+      ? `Objednávka z webu od ${userName}`
+      : `Zpráva z webu od ${userName}`,
+    html: htmlContent,
   };
 }
